bb0eac34193e9d98c9676fe6d6eaabb62c304930
Jayati Goud <jayatigoud123@gmail.com>
2023-03-06 01:39:53 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'de0a642555mshb15e8c21ccca557p1e5a8ejsn127d551ac3db',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5a6f99dd2ea4397f661d9a810c228f41";
